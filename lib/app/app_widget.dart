e63ef7042e3dbfe771711141a5dc738b6db950a3
Caio Reis <caio@codecity.dev>
2024-01-04 20:38:10 -0300

    Update MaterialApp configuration

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      key: key,
+      title: 'APOD',
+      routerConfig: Modular.routerConfig,
+    );
   }
 }
